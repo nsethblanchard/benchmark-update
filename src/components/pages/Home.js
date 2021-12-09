0b18bfc0fc09403162075e1d0de60ccd4004af35
Seth Blanchard <nsethblanchard@gmail.com>
2021-12-09 13:56:17 -0500

    styling

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,15 +1,31 @@
 import React from "react";
+import "./pages.css";
 
 function Home() {
   return (
-    <div>
-      <img
-        className="staunton"
-        src="/images/Staunton.png"
-        alt="Staunton city"
-      />
-      <h1>Home Page</h1>
-    </div>
+    <>
+      <div>
+        <img
+          className="staunton"
+          src="/images/Staunton.png"
+          alt="Staunton city"
+        />
+      </div>
+      <div className="page_content">
+        <h1 className="title">Employment Services in The Valley</h1>
+        <p className="mission">
+          BEST, LLC is a company devoted to offering top quality job search
+          services to public agencies who desire a great return on investment
+          for their clients. We offer an organized approach to finding the
+          correct solutions to employment barriers such as transportation,
+          daycare, lack of consistent job history, and general disorganization
+          that burden many low-income families. Our services are flexible and
+          can be molded to fit each individual’s background while maintaining a
+          structure that will result in quick success. We are a great link in
+          the chain to self-sufficiency and success in the workplace and life.{" "}
+        </p>
+      </div>
+    </>
   );
 }
 
